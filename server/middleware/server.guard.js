8df53267e6139b7159b49fc5f35b32ed4097f58d
omar <omarqais225@gmail.com>
2026-03-21 20:07:01 +0200

    save params from xss

--- server/middleware/server.guard.js
+++ server/middleware/server.guard.js
@@ -6,5 +6,8 @@ export const xss_protection = (request, response, next) => {
   for (let key in request.body) {
     request.body[key] = xss(request.body[key]);
   }
+  for (let key in request.params) {
+    request.params[key] = xss(request.params[key]);
+  }
   next();
-} 
+}

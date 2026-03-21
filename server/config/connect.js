f2865983dbcec7db041910cebe4614035c96a726
omar <omarqais225@gmail.com>
2026-03-21 18:52:44 +0200

    change colors

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import chalk from "chalk";
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("connect to Db success");
+    console.log(chalk.red('⚡'), chalk.green.bold(`MongoDb connected successfully`));
   } catch (error) {
     throw new Error(`${error instanceof Error ? error.message : String(error)}`);
   }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-import dotenv from "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
+import chalk from "chalk";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -8,13 +10,18 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
-connectToDB();
-app.listen(process.env.PORT, () => {
-        console.log(`Server running at: http://localhost:${process.env.PORT}`);
-});
+app.use("/auth", authRoutes);
 app.get("/test", (res, req) => {
   return res.status(200).json({
     success: true,
   });
 });
-app.use("/auth", authRoutes);
+connectToDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(chalk.green('✓'), chalk.blueBright.bold(`Server running at: http://localhost:${process.env.PORT}`));
+    console.log(chalk.yellow('★'), chalk.cyan(process.env.NODE_ENV == 'development' ? 'Ready for development' : 'Ready for using'));
+    console.log(chalk.yellow('DDoS protection activated for protected routes only'));
+});
+}).catch((error) => {
+  console.log(chalk.red(`${error}`));
+});

f1ff1cd4b0951ee1707dc20e0788edcbfe2d0ab2
omar <omarqais225@gmail.com>
2026-03-21 21:42:35 +0200

    protect routes

--- server/middleware/user.guard.js
+++ server/middleware/user.guard.js
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
+const getTokenData = (request) => {
+    const authHeader = request.headers['authorization'];
+    const token = request.cookies?.token || (authHeader && authHeader.split(' ')[1]);
+    if (!token) return null;
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        return null;
+    }
+};
 export const verifyUser = async (request, response, next) => {
     try {
-        const token = request.cookies?.token || request.headers['authorization']?.split(' ')[1];
-        if (!token) {
-            return response.status(401).json({
-                success: false,
-                error: '401 Unauthorized user'
-            });
-        }
-        let decoded;
-        try {
-            decoded = jwt.verify(token, process.env.JWT_SECRET);
-        } catch (err) {
+        const decoded = getTokenData(request);
+        if (!decoded) {
             return response.status(401).json({
                 success: false,
                 error: '401 Unauthorized user'
@@ -27,6 +28,23 @@ export const verifyUser = async (request, response, next) => {
         }
         request.user = { ...existingUser._doc, password: undefined };
         next();
+    } catch (error) {
+        return response.status(500).json({
+            success: false,
+            error: `Internal Server Error: ${error instanceof Error ? error.message : error}`
+        });
+    }
+};
+export const blockUser = async (request, response, next) => {
+    try {
+        const decoded = getTokenData(request);
+        if (!decoded) return next();
+        const existingUser = await User.findById(decoded.user._id);
+        if (!existingUser) return next();
+        return response.status(405).json({
+            success: false,
+            error: `405 Method Not Allowed You're already Login`
+        });
     } catch (error) {
         return response.status(500).json({
             success: false,
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { checkAuth, login, register } from "../controllers/auth.controller.js"; 
-import { verifyUser } from "../middleware/user.guard.js";
+import { blockUser, verifyUser } from "../middleware/user.guard.js";
 const router = express.Router();
-router.post("/login", login);
-router.post("/register", register);
+router.post("/login",blockUser,login);
+router.post("/register",blockUser,register);
 router.get("/check", verifyUser, checkAuth);
 export default router;

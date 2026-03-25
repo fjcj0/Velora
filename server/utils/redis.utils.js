c6627906d1a0438571dc029aadfd01d7fc33132b
Bassem <Bassemali@gmail.com>
2026-03-25 19:39:16 +0200

    add redis function

--- server/utils/redis.utils.js
+++ server/utils/redis.utils.js
@@ -1,14 +1,25 @@
 import { client } from "../config/redis.config.js";
-export const create = async () => {
-  try {
 
+export const create = async (key, value) => {
+  try {
+    await client.set(key, value, { EX: 60 });
   } catch (error) {
     throw error;
   }
 };
-export const get = async () => {
-  try {
 
+export const get = async (key) => {
+  try {
+    const value = await client.get(key);
+    return value;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteRedis = async (key) => {
+  try {
+    await client.del(key);
   } catch (error) {
     throw error;
   }

14743dbf8c35a8eeb9654e3a993edca2d4bdaf70
omar <omarqais225@gmail.com>
2026-04-09 03:21:24 +0300

    build layout

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
     return (
-        <div>
-         
+        <div className="w-screen h-screen flex bg-[#020711]">
+            <div className="w-[3.5rem] h-full bg-[#212121]">
+              
+            </div>
+            <div className="flex-1 p-5">
+                <div className="">
+
+                </div>
+                <div className="mt-3">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 }

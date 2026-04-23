4fbd36b33993241c9c11d7959792d18bc47217a5
omar <omarqais225@gmail.com>
2026-04-23 09:54:35 +0300

    deploy

--- server/routes/google.route.js
+++ server/routes/google.route.js
@@ -2,25 +2,14 @@ import express from "express";
 import passport from "../config/passport.config.js";
 import { googleCallback } from "../controllers/google.controller.js";
 import { blockUser } from "../middleware/user.guard.js";
-import { validateWhitelist } from "../middleware/server.guard.js";
 const router = express.Router();
 router.get(
   "/google",
-  validateWhitelist({
-    body: {},
-    query: {},
-    params: {}
-  }),
   blockUser,
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 router.get(
   "/google/callback",
-  validateWhitelist({
-    body: {},
-    query: {},
-    params: {}
-  }),
   blockUser,
   passport.authenticate("google", { session: false }),
   googleCallback

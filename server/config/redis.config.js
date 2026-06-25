d42b65eb9b1dcc6aaed03eb82aa47c35071f0918
omar <omarqais225@gmail.com>
2026-06-25 04:43:29 +0300

    use global redis in render

--- server/config/redis.config.js
+++ server/config/redis.config.js
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
 import chalk from "chalk";
 export const client = createClient({
-  url: "redis://localhost:6379", 
+  url: process.env.NODE_ENV === "development"
+    ? "redis://localhost:6379"
+    : process.env.REDIS_URL,
 });
 client.on("error", (err) => console.error(chalk.bgRed.white("Redis Error:"), err));
 export const connectToRedis = async () => {
